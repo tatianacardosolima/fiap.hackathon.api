656aca9661688bbd3a55e644b052b024fcfeb9ad
tatianacardosolima <tatidornel@gmail.com>
2025-02-07 20:18:37 -0300

    mongo deployment e docker file

--- src/Medicos/Fiap.Hackathon.Medicos.API/Program.cs
+++ src/Medicos/Fiap.Hackathon.Medicos.API/Program.cs
@@ -8,11 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<ExceptionFilter>(); // Adiciona o filtro globalmente
-});
+builder.Services.AddControllers();
+//builder.Services.AddControllers(options =>
+//{
+//    options.Filters.Add<ExceptionFilter>(); // Adiciona o filtro globalmente
+//});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -40,13 +40,12 @@ builder.Services.AddServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
